51a1c4d586e650915cc51087a798c6a73a6923ad
Reuben Cummings <reubano@gmail.com>
2013-05-17 02:15:23 +0300

    Fix tests

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -9,8 +9,10 @@
 import sys
 import nose.tools as nt
 
+from pprint import pprint
 from flask import json
-from app import create_app, db
+from app import create_app, db, models
+from app.helper import process, get_init_values, get_tables, get_keys
 
 loads = json.loads
 dumps = json.dumps
@@ -24,7 +26,6 @@ def setup_package():
 	global app
 	global client
 	global jsonx
-	global endpoint
 	global base
 
 	app = create_app(config_mode='Test')
@@ -54,11 +55,10 @@ def check_equal(page, x, y):
 
 
 def get_globals():
-	global app
 	global client
-	global jsonx
 
-	return app, client, jsonx
+	content = [process(v, get_keys()) for v in get_init_values()]
+	return client, get_tables(), content
 
 
 class APIHelper:
@@ -68,7 +68,7 @@ class APIHelper:
 		# returns status_code 200
 
 		if id:
-			url = base + table + '/' + id
+			url = base + table + '/' + str(id)
 		else:
 			url = base + table
 
@@ -81,7 +81,7 @@ class APIHelper:
 
 	def delete_data(self, table, id):
 		# returns status_code 204
-		url = '%s/%s/%s' % (endpoint, table, id)
+		url = '%s/%s/%s' % (base, table, id)
 		r = client.delete(url, content_type=self.json)
 		return r
 
@@ -94,7 +94,7 @@ class APIHelper:
 	def patch_data(self, data, table, id=None, query=None):
 		# returns status_code 200 or 201
 		if id:
-			url = base + table + '/' + id
+			url = base + table + '/' + str(id)
 		else:
 			url = base + table
 
--- app/tests/test_site.py
+++ app/tests/test_site.py
@@ -16,12 +16,11 @@ from app import create_app, db
 def setup_module():
 	"""site initialization"""
 	global initialized
-	global tables
 	global client
+	global tables
+	global content
 
-	app, client, jsonx = get_globals()
-	keys = [k for k in app.blueprints.keys() if k.endswith('api0')]
-	tables = [k.replace('api0', '') for k in keys]
+	client, tables, content = get_globals()
 	db.create_all()
 	initialized = True
 	print('Site Module Setup\n')
@@ -32,10 +31,53 @@ class TestAPI(APIHelper):
 	def __init__(self):
 		self.cls_initialized = False
 
+	def setUp(self):
+		"""database initialization"""
+		assert not self.cls_initialized
+		db.create_all()
+
+		for bundle in content:
+			for piece in bundle:
+				table = piece['table']
+				data = piece['data']
+
+				for d in data:
+					r = self.post_data(d, table)
+					nt.assert_equal(r.status_code, 201)
+
+		self.cls_initialized = True
+		print('\nTestAPI Class Setup\n')
+
+	def tearDown(self):
+		"""database removal"""
+		assert self.cls_initialized
+		db.drop_all()
+		self.cls_initialized = False
+
+		print('TestAPI Class Teardown\n')
+
 	def test_api_get(self):
 		for table in tables:
+			self.setUp()
 			n = self.get_num_results(table)
+			self.tearDown()
 			yield check_equal, table, n >= 0, True
+
+	def test_api_delete(self):
+		for table in tables:
+			self.setUp()
+			old = self.get_num_results(table)
+
+			if old > 0:
+				# delete first entry
+				r = self.delete_data(table, 1)
+
+				# test that the entry was deleted
+				new = self.get_num_results(table)
+				self.tearDown()
+				yield check_equal, table, new, old - 1
+			else:
+				self.tearDown()
 
 
 class TestWeb:
